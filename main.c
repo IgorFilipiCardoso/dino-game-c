1292e82a8b36a8b5b4b6f798b59d36af8d38b9e0
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-10-18 11:32:28 -0300

    Animação, Colisão, Chão, Gravidade

--- main.c
+++ main.c
@@ -3,26 +3,44 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-#define WIDTH 640 
+#define WIDTH 640
 #define HEIGHT 480
+#define GRAVITY 0.25
 
 typedef struct {
   int x, y;
-  SDL_Texture* player_texture;
+  int height, width;
+  SDL_Texture* player_texture1;
+  SDL_Texture* player_texture2;
+  SDL_Texture* player_texture3;
+  bool step;
+  bool pulando;
+  double velocity;
 } Player;
 
 typedef struct {
-  SDL_Texture* map_texture;
   int x, y;
+  int height, width;
+  SDL_Texture* cactus_texture;
+} Cactus;
+
+typedef struct {
+  int x, y;
+  int height, width;
+  SDL_Texture* map_texture;
 } Map;
 
 typedef struct {
   Player player;
+  Cactus cactus;
   Map map;
 } GameState;
 
-int processEvents(SDL_Window* window, GameState* gameState);
+int processEvents(SDL_Window* window, GameState* gameState, SDL_Renderer* renderer);
 void doRender(SDL_Renderer* renderer, GameState* gameState);
+bool areColliding(GameState gameState);
+void processTextures(SDL_Renderer* renderer, SDL_Texture** texture, char* src);
+void player_render(GameState* gameState, SDL_Renderer* renderer);
 
 int main(int argc, char* argv[])
 {
@@ -32,52 +50,53 @@ int main(int argc, char* argv[])
   SDL_Renderer* render = NULL;
   SDL_Surface* player_texture_surface = NULL;
   SDL_Surface* map_img_surface = NULL;
+  SDL_Surface* cactus_text_surface = NULL;
 
   GameState gameState;
 
   gameState.map.x = 0;
   gameState.map.y = 0;
+  gameState.map.height = HEIGHT * 2;
+  gameState.map.width = WIDTH * 2;
 
-  gameState.player.x = 0;
+  gameState.player.x = 25;
   gameState.player.y = 300;
+  gameState.player.height = 64;
+  gameState.player.width = 60;
+  gameState.player.velocity = 0;
+  gameState.player.step = false;
+
+  gameState.cactus.x = WIDTH;
+  gameState.cactus.y = 300;
+  gameState.cactus.width = 54;
+  gameState.cactus.height = 64;
 
   SDL_Init(SDL_INIT_VIDEO);
 
   window = SDL_CreateWindow("Jogo mais foda do mes", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, 0);
   render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-  player_texture_surface = IMG_Load("./images/t_rex.png");
-  if (player_texture_surface == NULL) {
-    printf("Imagem do 'player' nao encontrada!");
-    SDL_Quit();
-    return 1;
-  }
+  processTextures(render, &gameState.player.player_texture1, "./images/t_rex1.png");
+  processTextures(render, &gameState.player.player_texture2, "./images/t_rex2.png");
+  processTextures(render, &gameState.player.player_texture3, "./images/t_rex3.png");
 
-  map_img_surface = IMG_Load("./images/map.png");
-  if (map_img_surface == NULL)
-  {
-    printf("Imagem do 'mapa' nao encontrada!");
-    SDL_Quit();
-    return 1;
-  }
+  processTextures(render, &gameState.map.map_texture, "./images/map.png");
+  processTextures(render, &gameState.cactus.cactus_texture, "./images/cactus.png");
 
-  gameState.map.map_texture = SDL_CreateTextureFromSurface( render, map_img_surface);
-  gameState.player.player_texture = SDL_CreateTextureFromSurface( render, player_texture_surface);
-
-  SDL_FreeSurface(map_img_surface);
-  SDL_FreeSurface(player_texture_surface);
 
   while (!stop) {
-    stop = processEvents(window, &gameState);
+    stop = processEvents(window, &gameState, render);
     doRender(render, &gameState);
 
-    printf("%d\n", gameState.player.y);
+    if (areColliding(gameState)) {
+      printf("Relou\n");
+    }
 
     SDL_Delay(10);
   }
 
   SDL_DestroyTexture(gameState.map.map_texture);
-  SDL_DestroyTexture(gameState.player.player_texture);
+  SDL_DestroyTexture(gameState.player.player_texture1);
 
   SDL_DestroyWindow(window);
   SDL_DestroyRenderer(render);
@@ -86,15 +105,37 @@ int main(int argc, char* argv[])
   return 0;
 }
 
+void processTextures(SDL_Renderer* renderer, SDL_Texture** texture, char* src)
+{
+  SDL_Surface* surface;
 
-int processEvents(SDL_Window* window, GameState* gameState)
+  surface = IMG_Load(src);
+  if (surface == NULL) {
+    printf("Imagem nao encontrada!");
+    SDL_Quit();
+    exit(1);
+  }
+
+  *texture = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_FreeSurface(surface);
+}
+
+int processEvents(SDL_Window* window, GameState* gameState, SDL_Renderer* renderer)
 {
   SDL_Event event;
   bool stop;
 
-  if (gameState->player.y < 300)
-    gameState->player.y += 2;
-  
+  // Gravidade
+  if (gameState->player.y < 300) {
+    gameState->player.velocity += GRAVITY;
+    gameState->player.y += gameState->player.velocity;
+    if (gameState->player.y > 300)
+      gameState->player.y = 300;
+  }
+
+  if (gameState->player.y == 300)
+    gameState->player.velocity = 0;
+
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
       case SDL_WINDOWEVENT_CLOSE:
@@ -110,45 +151,85 @@ int processEvents(SDL_Window* window, GameState* gameState)
             stop = true;
             break;
           case SDLK_UP:
-            gameState->player.y -= 100;
+            if (gameState->player.y == 300) {
+              for (int i = 0; gameState->player.y > 135; i++) {
+                gameState->player.y -= 10;
+                doRender(renderer, gameState);
+                SDL_Delay(10);
+              }
+            }
+
             break;
         }
         break;
       case SDL_QUIT:
         stop = true;
         break;
-    }
 
+    }
   }
-  
-  if (gameState->map.x == -480)
-      gameState->map.x = 0;
-    
-    gameState->map.x -= 3;
 
   return stop;
 }
 
 void doRender(SDL_Renderer* renderer, GameState* gameState)
 {
+
+  if (gameState->cactus.x < -gameState->cactus.width)
+    gameState->cactus.x = WIDTH;
+
+  gameState->cactus.x -= 4;
+
+  if (gameState->map.x < -gameState->map.width / 2)
+    gameState->map.x = 0;
+
+  gameState->map.x -= 4;
+
   SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
 
   SDL_RenderClear(renderer);
 
   SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-  SDL_Rect mapRect = { gameState->map.x, gameState->map.y, WIDTH*2, HEIGHT};
+  SDL_Rect mapRect = { gameState->map.x, gameState->map.y, gameState->map.width, gameState->map.height };
   SDL_RenderCopy(renderer, gameState->map.map_texture, NULL, &mapRect);
 
-  SDL_Rect playerRect = {gameState->player.x, gameState->player.y, 64, 64};
-  SDL_RenderCopy(renderer, gameState->player.player_texture, NULL, &playerRect);
+  SDL_Rect rect = { 0, gameState->cactus.y + gameState->cactus.height, WIDTH, 300 / 2 };
+  SDL_RenderFillRect(renderer, &rect);
+
+  SDL_Rect cactusRect = { gameState->cactus.x, gameState->cactus.y, gameState->cactus.width, gameState->cactus.height };
+  SDL_RenderCopy(renderer, gameState->cactus.cactus_texture, NULL, &cactusRect);
+
+  player_render(gameState, renderer);
 
   SDL_RenderPresent(renderer);
 }
 
-bool areColliding(GameState gameState){
-  if (gameState.player.x == gameState.map.x || gameState.player.y == gameState.map.y)
-    return true;
-  
-  return false;
+void player_render(GameState* gameState, SDL_Renderer* renderer)
+{
+  if (gameState->player.y == 300) {
+    if (gameState->player.step) {
+      SDL_Rect playerRect = { gameState->player.x, gameState->player.y, gameState->player.width, gameState->player.height };
+      SDL_RenderCopy(renderer, gameState->player.player_texture1, NULL, &playerRect);
+      gameState->player.step = false;
+    } else {
+      SDL_Rect playerRect = { gameState->player.x, gameState->player.y, gameState->player.width, gameState->player.height };
+      SDL_RenderCopy(renderer, gameState->player.player_texture2, NULL, &playerRect);
+      gameState->player.step = true;
+    }
+  } else {
+    SDL_Rect playerRect = { gameState->player.x, gameState->player.y, gameState->player.width, gameState->player.height };
+    SDL_RenderCopy(renderer, gameState->player.player_texture1, NULL, &playerRect);
+  }
+}
+
+bool areColliding(GameState gameState)
+{
+
+  if (gameState.player.x + gameState.player.width / 2 < gameState.cactus.x - gameState.cactus.width / 2) return false;
+  if (gameState.player.x - gameState.player.width / 2 > gameState.cactus.x + gameState.cactus.width / 2) return false;
+  if (gameState.player.y + gameState.player.height / 2 < gameState.cactus.y - gameState.cactus.height / 2) return false;
+  if (gameState.player.y - gameState.player.height / 2 > gameState.cactus.y + gameState.cactus.height / 2) return false;
+
+  return true;
 }
