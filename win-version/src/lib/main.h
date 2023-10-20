b867265cb8b5eb0b91ea50a37762db7affd87d74
Igor.Cardoso <igorfilipicardoso@gmail.com>
2023-10-20 17:32:35 -0300

    Arrumando os diretorios

--- win-version/src/lib/main.h
+++ win-version/src/lib/main.h
@@ -1,6 +1,6 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
+#include "SDL2/include/SDL2/SDL.h"
+#include "SDL2/include/SDL2/SDL_image.h"
+#include "SDL2/include/SDL2/SDL_mixer.h"
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
